d191c2fb54f8c57a76004be76fd9fba3e6a516ae
ficapy <c4d@outlook.com>
2018-01-21 16:54:25 +0800

    第一版

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: ficapy
+# Create: '21/01/2018'
+
+import os
+from apscheduler.schedulers.blocking import BlockingScheduler
+from telegram import Bot, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
+from hackernews import HackerNews
+
+token = os.getenv("TOKEN")
+assert token
+
+hn = HackerNews()
+bot = Bot(token)
+
+scheduler = BlockingScheduler(timezone="Asia/Shanghai")
+
+
+@scheduler.scheduled_job('cron', id='telegram', hour=9)
+def message():
+    print("Send Message")
+    for story_id in hn.top_stories(limit=10):
+        item = hn.get_item(story_id)
+
+        score = InlineKeyboardButton(text="Score: {}".format(item.score), url=item.url)
+        url = InlineKeyboardButton(text="Comments: {}".format(item.descendants),
+                                   url="https://news.ycombinator.com/item?id={}".format(item.item_id))
+        reply_markup = InlineKeyboardMarkup([[score, url]])
+        bot.sendMessage(chat_id="@MyHN_Bot",
+                        text="*{text}* [{url}]({url})".format(text=item.title, url=item.url),
+                        parse_mode=ParseMode.MARKDOWN, reply_markup=reply_markup)
+
+
+scheduler.start()
